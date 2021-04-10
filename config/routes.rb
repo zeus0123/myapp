bdb9c1cce676ee904a6430a0a6fc218d8091ef67
Trishan <ghoshtrishan73@gmail.com>
2021-04-10 19:41:35 +0530

    installed bcrypt, user model and sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # about page
   get "about", to: "about#index"
 
+  # Sign Up Page
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
