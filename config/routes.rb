3b36ce298c98f97bdcc88ca73ec30e72ab56c039
Trishan <ghoshtrishan73@gmail.com>
2021-04-13 21:58:44 +0530

    login and sign up done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Sign Up Page
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  # Sign In Pagerai
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
