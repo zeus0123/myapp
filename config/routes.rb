aae2923f389e94d5128ada2bf1d0099661ec3b50
Trishan <ghoshtrishan73@gmail.com>
2021-04-26 21:16:10 +0530

    forget password and rails twitter credentials

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,11 @@ Rails.application.routes.draw do
 
   delete "logout", to: "sessions#destroy"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
   root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
